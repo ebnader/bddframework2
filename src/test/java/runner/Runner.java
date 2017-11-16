937ddbbe8a9f83a081eaaf6e44a334bdd02a14f2
myusername <ebnader@live.com>
2017-11-16 11:02:46 -0500

    hello

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,12 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\ebnad\\workspace2\\bddframework2\\src\\test\\java\\feature\\TagForFeature.feature",glue="")
+public class Runner {
+
+}
